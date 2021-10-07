57fa12ffafa97ef73ec7e3c87a1b2eee6496e3f7
Jairo Sanchez <69741616+sanchezjairo@users.noreply.github.com>
2021-10-07 14:27:25 -0700

    Implemented search pagination

--- models/pet.js
+++ models/pet.js
@@ -3,6 +3,12 @@
 const mongoose = require('mongoose'),
         Schema = mongoose.Schema;
 
+const mongoosePaginate = require('mongoose-paginate');
+
+mongoosePaginate.paginate.options = {
+  limit: 3 // how many records on each page
+        };
+
 const PetSchema = new Schema({
     name            : { type: String, required: true }
   , species         : { type: String }
@@ -16,4 +22,8 @@ const PetSchema = new Schema({
   timestamps: true
 });
 
+module.exports = mongoose.model('Pet', PetSchema);
+PetSchema.plugin(mongoosePaginate);
+
+// This line is the same as before
 module.exports = mongoose.model('Pet', PetSchema);
